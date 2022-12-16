de0a68c6b2f88870f18f8a3be27696e778017638
Katarzyna Wasilewska <kwasilewska2014@gmail.com>
2022-12-16 08:38:20 +0100

    add changes

--- server.js
+++ server.js
@@ -5,24 +5,30 @@ const app = express();
 
 app.use((req, res, next) => {
   res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
+    res.sendFile(path.join(__dirname, `./public/${name}`));
+  },
   next();
 });
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/user', (req, res) => {
+  res.show('You have to logeed in');
+});
 
 app.get('/', (req, res) => {
-  res.show('index.html');
-});
+    res.show('home.html');
+  });
+
+  app.get('/home', (req, res) => {
+    res.show('home.html');
+  });
 
 app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
 app.use((req, res) => {
-  res.status(404).send('404 not found...');
-})
+  res.status(404).sendFile(path.join(__dirname, '404.jpg'));
+});
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
